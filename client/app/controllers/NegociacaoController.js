61da7a3796d73c5f92716728e55e386578d5565a
Mathias Artur Schulz <mathiasschulz34@gmail.com>
2020-11-12 13:29:49 -0300

    [ADD] - Capitulo 6

--- client/app/controllers/NegociacaoController.js
+++ client/app/controllers/NegociacaoController.js
@@ -7,6 +7,8 @@ class NegociacaoController {
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
         this._negociacoes = new Negociacoes();
+        this._negociacoesView = new NegociacoesView('#negociacoes');
+        this._negociacoesView.update();
     }
 
     adiciona(event) {
