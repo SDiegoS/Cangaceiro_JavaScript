fd57b4e81680ecdfb77a2a4aea12a404e9a3fb3f
Mathias Artur Schulz <mathiasschulz34@gmail.com>
2020-11-12 10:10:17 -0300

    [ADD] - Capitulo 4 concluido.

--- client/app/controllers/NegociacaoController.js
+++ client/app/controllers/NegociacaoController.js
@@ -11,18 +11,17 @@ class NegociacaoController {
     adiciona(event) {
         event.preventDefault();
 
-        let data = new Date(...
-            this._inputData.value
-                .split('-')
-                .map((item, indice) => item - indice % 2)
-        );
+        // let data = converter.paraData(this._inputData.value)
 
-        //	precisamos	acessar	as	propriedades	através	de	this
         let negociacao = new Negociacao(
-            data,
+            DateConverter.paraData(this._inputData.value),
             parseInt(this._inputQuantidade.value),
             parseFloat(this._inputValor.value)
         );
+
         console.log(negociacao);
+
+        let diaMesAno = DateConverter.paraTexto(negociacao.data);
+        console.log(diaMesAno);
     }
 }
