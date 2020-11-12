b18408afdfad68bec1b5e420ea8175b3d7f6d5ed
Mathias Artur Schulz <diego_ireno@hotmail.com>
2020-11-12 15:47:16 -0300

    [ADD] - Capitulo 6 finalizado.

--- client/app/domain/negociacao/Negociacoes.js
+++ client/app/domain/negociacao/Negociacoes.js
@@ -12,16 +12,8 @@ class Negociacoes {
     }
 
     get volumeTotal() {
-        // let total = 0;
-        //
-        // for (let i = 0; i < this._negociacoes.length; i++) {
-        //     total += this._negociacoes[i].volume;
-        // }
-        // return total;
 
         return this._negociacoes
-            .reduce(function (total, negociacao) {
-                return total + negociacao.volume
-            }, 0);
+            .reduce((total, negociacao) =>  total + negociacao.volume, 0);
     }
 }
