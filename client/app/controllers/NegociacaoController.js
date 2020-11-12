ade9a7b5cc0cdec8b2e38f97d521416e3a1caa2e
Mathias Artur Schulz <mathiasschulz34@gmail.com>
2020-11-12 11:38:03 -0300

    [ADD] - Capitulo 5 Concluido.

--- client/app/controllers/NegociacaoController.js
+++ client/app/controllers/NegociacaoController.js
@@ -6,22 +6,28 @@ class NegociacaoController {
         this._inputData = $('#data');
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
+        this._negociacoes = new Negociacoes();
     }
 
     adiciona(event) {
         event.preventDefault();
+        this._negociacoes.adiciona(this._criaNegociacao());
+        this._limpaFormulario();
+    }
 
-        // let data = converter.paraData(this._inputData.value)
+    _limpaFormulario() {
+        this._inputData.value = '';
+        this._inputQuantidade.value = 1;
+        this._inputValor.value = 0.0
+        this._inputData.focus();
+    }
 
-        let negociacao = new Negociacao(
+    _criaNegociacao() {
+        //	retorna	uma	instância	de	negociação
+        return new Negociacao(
             DateConverter.paraData(this._inputData.value),
             parseInt(this._inputQuantidade.value),
             parseFloat(this._inputValor.value)
         );
-
-        console.log(negociacao);
-
-        let diaMesAno = DateConverter.paraTexto(negociacao.data);
-        console.log(diaMesAno);
     }
 }
