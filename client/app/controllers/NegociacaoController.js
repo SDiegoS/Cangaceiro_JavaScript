5a26ade7f9824ee8627eff6b8cfa96263c352fa7
Diego Ireno dos Santos <diego@conectra.net.br>
2020-11-24 11:33:18 -0300

    [ADD] - Finalizado capitulo 11.

--- client/app/controllers/NegociacaoController.js
+++ client/app/controllers/NegociacaoController.js
@@ -1,49 +1,59 @@
 class NegociacaoController {
 
-    constructor() {
-        const $ = document.querySelector.bind(document);
-        this._inputData = $('#data');
-        this._inputQuantidade = $('#quantidade');
-        this._inputValor = $('#valor');
+  constructor () {
+    const $ = document.querySelector.bind(document);
+    this._inputData = $('#data');
+    this._inputQuantidade = $('#quantidade');
+    this._inputValor = $('#valor');
 
-        this._negociacoes = new Bind(
-            new Negociacoes(),
-            new NegociacoesView('#negociacoes'),
-            'adiciona', 'esvazia'
-        );
+    this._negociacoes = new Bind(
+      new Negociacoes(),
+      new NegociacoesView('#negociacoes'),
+      'adiciona', 'esvazia'
+    );
 
-        this._mensagem = new Bind(
-            new Mensagem(),
-            new MensagemView('#mensagemView'),
-            'texto'
-        );
+    this._mensagem = new Bind(
+      new Mensagem(),
+      new MensagemView('#mensagemView'),
+      'texto'
+    );
+  }
+
+  adiciona (event) {
+    try {
+      event.preventDefault();
+      this._negociacoes.adiciona(this._criaNegociacao());
+      this._mensagem.texto = 'Negociação adicionada com sucesso';
+      this._limpaFormulario();
+    } catch (err) {
+      console.log(err);
+      console.log(err.stack);
+      if (err instanceof DataInvalidaException) {
+        this._mensagem.texto = err.message;
+      } else {
+        this._mensagem.texto = 'Um erro não esperado aconteceu. Entre em contato com o suporte';
+      }
     }
+  }
 
-    adiciona(event) {
-        event.preventDefault();
-        this._negociacoes.adiciona(this._criaNegociacao());
-        this._mensagem.texto = 'Negociação adicionada com sucesso';
-        this._limpaFormulario();
-    }
+  _limpaFormulario () {
+    this._inputData.value = '';
+    this._inputQuantidade.value = 1;
+    this._inputValor.value = 0.0;
+    this._inputData.focus();
+  }
 
-    _limpaFormulario() {
-        this._inputData.value = '';
-        this._inputQuantidade.value = 1;
-        this._inputValor.value = 0.0
-        this._inputData.focus();
-    }
+  _criaNegociacao () {
+    //	retorna	uma	instância	de	negociação
+    return new Negociacao(
+      DateConverter.paraData(this._inputData.value),
+      parseInt(this._inputQuantidade.value),
+      parseFloat(this._inputValor.value)
+    );
+  }
 
-    _criaNegociacao() {
-        //	retorna	uma	instância	de	negociação
-        return new Negociacao(
-            DateConverter.paraData(this._inputData.value),
-            parseInt(this._inputQuantidade.value),
-            parseFloat(this._inputValor.value)
-        );
-    }
-
-    apaga() {
-        this._negociacoes.esvazia();
-        this._mensagem.texto = 'Negociações apagadas com sucesso';
-    }
+  apaga () {
+    this._negociacoes.esvazia();
+    this._mensagem.texto = 'Negociações apagadas com sucesso';
+  }
 }
--- client/app/ui/converters/DateConverter.js
+++ client/app/ui/converters/DateConverter.js
@@ -1,22 +1,23 @@
 class DateConverter {
 
-    constructor() {
-        throw new Error('Esta classe não pode ser instanciada');
+  constructor () {
+    throw new Error('Esta classe não pode ser instanciada');
+  }
+
+  static paraTexto (data) {
+    return `${data.getDate()}/${data.getMonth() + 1}/${data.getFullYear()}`;
+  }
+
+  static paraData (texto) {
+
+    if (!/\d{2}\/\d{2}\/\d{4}/.test(texto)) {
+      throw new DataInvalidaException();
     }
 
-    static paraTexto(data) {
-        return `${data.getDate()}/${data.getMonth() + 1}/${data.getFullYear()}`;
-    }
-
-    static paraData(texto) {
-
-        if (!/^\d{4}-\d{2}-\d{2}$/.test(texto)) {
-            throw    new Error('Deve	estar	no	formato	aaaa-mm-dd');
-        }
-
-        return new Date(...texto
-            .split('-')
-            .map((item, indice) => item - indice % 2)
-        );
-    }
+    return new Date(...texto
+      .split('/')
+      .reverse()
+      .map((item, indice) => item - indice % 2)
+    );
+  }
 }
